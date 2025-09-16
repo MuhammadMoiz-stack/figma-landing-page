7f36e98c35d1c7e48422f8263e4d1238b9d14043
Muhammad Moiz <reachmoizakhter@gmail.com>
2025-09-16 12:32:59 -0800

    add app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("-------------------------")
+console.log("My Name Is Muhammad Moiz.")
+console.log("-------------------------")
+
+function showsidebar() {
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "flex"
+    
+}
+
+function hidesidebar() {
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "none"
+}
